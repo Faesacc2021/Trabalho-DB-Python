a0bf255ddf8622f7a87701d058eab29e06126ede
Alunos CC Faesa 02 2021 <103604848+Faesacc2021@users.noreply.github.com>
2022-10-15 12:17:53 -0300

    Dump Julia

---
+++ Dump_julia/beautysalon_loginuser.sql
@@ -0,0 +1,37 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: beautysalon
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Dumping data for table `loginuser`
+--
+
+LOCK TABLES `loginuser` WRITE;
+/*!40000 ALTER TABLE `loginuser` DISABLE KEYS */;
+INSERT INTO `loginuser` VALUES (1,'Heitor','123456','','','',''),(2,'mateuspaulo@gmail.com','159630','','','',''),(3,'julia@gmail.com','159630','','','',''),(4,'julia@gmail.com','159630','','','',''),(5,'julia@gmail.com','159630','','','',''),(6,'julia@gmail.com','159630','','','',''),(7,'julia@gmail.com','159630','','','',''),(8,'julia@gmail.com','159630','','','',''),(9,'julia@gmail.com','159630','','','',''),(10,'Franz@gmail.com','159630','','','',''),(11,'Carla@gmail.com','159630','','','',''),(12,'Carla@gmail.com','159630','','','',''),(13,'Carla@gmail.com','159630','','','',''),(14,'Carla@gmail.com','159630','','','','');
+/*!40000 ALTER TABLE `loginuser` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-15 12:15:58
